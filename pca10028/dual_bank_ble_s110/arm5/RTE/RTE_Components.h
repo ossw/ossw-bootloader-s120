356d7a4c07ebe52dcee1a9136167951eaa90c609
Krzysztof Szczesniak <k.szczesniak@althink.com>
2015-04-03 21:14:37 +0200

    initial commit - dual_bank_ble_s110 sample from 8.0.0 SDK

---
+++ pca10028/dual_bank_ble_s110/arm5/RTE/RTE_Components.h
@@ -0,0 +1,17 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'dfu_dual_bank_ble_s110_pca10028' 
+ * Target:  'nrf51422_xxac' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define BSP_DEFINES_ONLY
+#define S110
+#define SOFTDEVICE_PRESENT
+
+#endif /* RTE_COMPONENTS_H */
